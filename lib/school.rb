74230d128a06b4ca12200ad1f12379089054ef8f
Michael Ernest <m.hendersun@gmail.com>
2020-10-19 15:37:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+
+class School
+  attr_accessor :student, :class, :grade
+
+  GRADES = []
+
+  def initialize(student, class, grade)
+    @student = student
+    @class = class
+    @grade = grade
+  end 
+  
+end 
+
+school = School.new("Bayside High School")
