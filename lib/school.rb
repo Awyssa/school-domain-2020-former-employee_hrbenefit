a4073c29d661dc46163d4fbaea47cfd7294cda88
Michael Ernest <m.hendersun@gmail.com>
2020-10-19 15:46:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,22 @@ class School
     roster = {}
   end
 
+  def add_student(student_name, grade)
+    roster << studnet_name
+    roster[student_name] << grade
+    # roster[grade] ||= []
+    # roster[grade] << student_name
+  end
+
+
+
+  
 end
+
+
+
+
+
+
 
 school = School.new("Bayside High School")
